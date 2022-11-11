aae15b3067bc209f2691c949276a3bc0b0dfa82c
makhan0011 <112144798+makhan0011@users.noreply.github.com>
2022-11-11 00:51:54 +0000

    c

--- q2.cpp
+++ q2.cpp
@@ -2,13 +2,44 @@
 #include <iomanip>
 using namespace std;
 
-// ******************************
-// Your code here
-// ******************************
+//initiating the functions
+void getInput(int &n1, int &n2, int &n3);
+int  findMin(int n1, int n2, int n3);
+void printResult(int n1, int n2, int n3, int min);
 
-// ******************************
-// this function will help you pass the test. When you print the result with this function, you will get the same output format
+int main()
+{
+	//set up to declare the variables & setting up for input and output
+	int n1,n2,n3,min;
+	getInput(n1,n2,n3);
+	min=findMin(n1,n2,n3);
+	printResult(n1,n2,n3,min);
+}
+
+//setting up the console output
 void printResult(int n1, int n2, int n3, int min)
 {
 	cout << n1 << " " << n2 << " " << n3 << " " << min << " " << endl;
+}
+void getInput(int &n1, int &n2, int &n3)
+{
+	cout<<"Please Enter Three integer: "<<endl;
+	cin>>n1>>n2>>n3;
+}
+//setting up to find min using if else statements
+int  findMin(int n1, int n2, int n3)
+{
+	int min=n1;
+	if(n2<n1)
+	{
+		min=n2;
+		if(n3<n2)
+			min=n3;
+	}
+	else
+	{
+		if(n3<n1)
+			min=n3;
+	}
+	return min;
 }
